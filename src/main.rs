fb1979a4516a6a6d930eb6db4616addfc23b05b5
Denatle <iq.labozin@gmail.com>
2024-03-05 20:22:35 +0300

    better CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs, io};
 use std::path::PathBuf;
-use walkdir::{DirEntry, WalkDir};
-use rand::Rng;
+use std::time::Instant;
+use walkdir::{WalkDir};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -12,54 +12,32 @@ struct Cli {
 // const DIR: &str = "E:/testland/";
 
 fn main() -> io::Result<()> {
+    let now = Instant::now();
     let args = Cli::parse();
     let path = args.path;
-    // rename_dirs(path.clone()).unwrap();
-    // rename_files(path).unwrap();
+    rename_tree(path.clone()).unwrap();
+    let now2 = now.elapsed();
+    println!("{:?}", now2);
+    println!("Took {} seconds", now2.as_secs());
     Ok(())
 }
 
-fn rename_files(start_directory: PathBuf) -> io::Result<()> {
-   for entry in WalkDir::new(start_directory)
-        .contents_first(true).min_depth(1)
-        .into_iter()
-        .filter_entry(is_file) {
-        let path = entry.unwrap().into_path();
-        println!("old: {}", path.to_str().unwrap());
-        let new_path = get_new_path(path.clone());
-        println!("new: {}\n", new_path);
-        rename(path, PathBuf::from(new_path));
-    }
-    Ok(())
-}
-
-fn rename_dirs(start_directory: PathBuf) -> io::Result<()> {
+fn rename_tree(start_directory: PathBuf) -> io::Result<()> {
     for entry in WalkDir::new(start_directory)
         .contents_first(true).min_depth(1)
-        .into_iter()
-        .filter_entry(|e| !is_file(e)) {
+        .into_iter() {
         let path = entry.unwrap().into_path();
-        println!("old: {}", path.to_str().unwrap());
         let new_path = get_new_path(path.clone());
-        println!("new: {}\n", new_path);
         rename(path, PathBuf::from(new_path));
     }
     Ok(())
 }
 
 fn get_new_path(path: PathBuf) -> String {
-    let mut rng = rand::thread_rng();
-    let random: u8 = rng.gen();
     format!("{}/{:?}", path.parent().unwrap().to_str().unwrap(), md5::compute(
-        format!("{}{}", path.file_name().unwrap().to_str().unwrap(), random)))
+        path.file_name().unwrap().to_str().unwrap()))
 }
 
 fn rename(path: PathBuf, new_path: PathBuf) {
-    if fs::rename(path.clone(), new_path.clone()).is_err() && fs::rename(path.clone(), new_path.clone()).is_err() {
-        println!("Error!\n\n")
-    }
-}
-
-fn is_file(entry: &DirEntry) -> bool {
-    entry.file_type().is_file()
+    let _ = fs::rename(path.clone(), new_path.clone()).is_err();
 }
